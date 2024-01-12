54fe268c91b8cc610e0ddd6834e31326bff76f44
gubod1003 <gubod1003@gmail.com>
2024-01-12 10:28:57 +0300

    timer
    Add time

--- garland/garland.js
+++ garland/garland.js
@@ -6,12 +6,16 @@ const garlandInit = () => {
 
     const elka = document.createElement('div');
     elka.classList.add('garland-elka');
+/*For second garland - start*/
+    const elkaRight = document.createElement('div');
+    elkaRight.classList.add('garland-elka', 'garland-elka_right');
+/*For second garland - end*/
 
     const garland = document.createElement('div');
     garland.className = 'garland garland_1';
 
     document.head.append(link);
-    document.body.append(elka, garland);
+    document.body.append(elka, elkaRight, garland);
 
     const garlandClasses = ['garland_1', 'garland_2', 'garland_3', 'garland_4]'];
     let currentIndex = 0;
