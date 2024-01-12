0c57a8dba11bccbe025411a7204da027b4812617
gubod1003 <gubod1003@gmail.com>
2024-01-12 11:39:39 +0300

    consultation
    Add modal consultation

--- script/modal.js
+++ script/modal.js
@@ -1,7 +1,13 @@
 const buttonsOrder = document.querySelectorAll('.product__button_order');
+const buttonsConsultation = document.querySelectorAll('.product__button_more');
 const overlayOrder = document.querySelector('.overlay_order');
+const overlayConsultation = document.querySelector('.overlay_consultation');
+const buttonGeneralOrder = document.querySelector('.hero__down')
+const buttonGeneralConsultation = document.querySelector('.header__consultation');
 
 
+
+buttonsConsultation.header__consultation = document.querySelector('.header__consultation');
 
 buttonsOrder.forEach(buttonOrder => {
     buttonOrder.addEventListener('click', () => {
@@ -11,10 +17,38 @@ buttonsOrder.forEach(buttonOrder => {
     })
 });
 
+buttonGeneralOrder.addEventListener('click', () => {
+    overlayOrder.classList.add('overlay_active');
+    const order = overlayOrder.querySelector('.modal__order');
+    order.value = buttonGeneralOrder.dataset.order;
+});
+
+buttonsConsultation.forEach(buttonConsultation => {
+    buttonConsultation.addEventListener('click', () => {
+        overlayConsultation.classList.add('overlay_active');
+        const consultation = overlayConsultation.querySelector('.modal__consultation');
+        consultation.value = buttonConsultation.dataset.consultation;
+    })
+})
+
+buttonGeneralConsultation.addEventListener('click', () => {
+    overlayConsultation.classList.add('overlay_active');
+    const consultation = overlayConsultation.querySelector('.modal__consultation');
+    consultation.value = buttonGeneralConsultation.dataset.consultation;
+});
+
 overlayOrder.addEventListener('click', event => {
     const target = event.target;
     if (target === overlayOrder || target.closest('.modal__close')) {
         overlayOrder.classList.remove('overlay_active');
+    }
+    
+});
+
+overlayConsultation.addEventListener('click', event => {
+    const target = event.target;
+    if (target === overlayConsultation || target.closest('.modal__close')) {
+        overlayConsultation.classList.remove('overlay_active');
     }
     
 });
